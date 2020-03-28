3816254d38fbfa2bb816fd8faa5f6bfcaabebc59
Andrew Brainerd <drwb333@gmail.com>
2020-03-28 12:16:49 -0400

    Updates

---
+++ precache-manifest.6c5b4024ff84a70abcd6f20b4c4aa65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20081192b7c4e55e4804dda6ef5aa016",
+    "url": "/andrew-brainerd.github.io/michiganopoly/index.html"
+  },
+  {
+    "revision": "505db088dd62f6b78e3b",
+    "url": "/andrew-brainerd.github.io/michiganopoly/static/css/main.985e136c.chunk.css"
+  },
+  {
+    "revision": "0bcdcabf0396a1d01f8e",
+    "url": "/andrew-brainerd.github.io/michiganopoly/static/js/2.6754b8cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/andrew-brainerd.github.io/michiganopoly/static/js/2.6754b8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505db088dd62f6b78e3b",
+    "url": "/andrew-brainerd.github.io/michiganopoly/static/js/main.11505c45.chunk.js"
+  },
+  {
+    "revision": "91a12b9acd2e3730dab2",
+    "url": "/andrew-brainerd.github.io/michiganopoly/static/js/runtime-main.e54a8720.js"
+  }
+]);
