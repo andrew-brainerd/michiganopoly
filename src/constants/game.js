7f9af1dd7daa7258943829c662ffe881923a2836
Andrew Brainerd <drwb333@gmail.com>
2020-03-28 10:47:39 -0400

    Move spaces to constant structure

---
+++ src/constants/game.js
@@ -0,0 +1,90 @@
+export const spaces = [
+  {
+    key: 'go',
+    name: 'Go',
+    isCornerSpace: true,
+    column: 11,
+    row: 11
+  },
+  {
+    key: 'freeParking',
+    name: 'Free Parking',
+    isCornerSpace: true,
+    column: 1,
+    row: 1
+  },
+  {
+    key: 'goToJail',
+    name: 'Go To Jail',
+    isCornerSpace: true,
+    column: 11,
+    row: 1
+  },
+  {
+    key: 'corunna',
+    name: 'Corunna',
+    color: 'brown',
+    column: 10,
+    row: 11
+  },
+  {
+    key: 'communityChestBottom',
+    name: 'Community Chest',
+    column: 9,
+    row: 11
+  },
+  {
+    key: 'owosso',
+    name: 'Owosso',
+    color: 'brown',
+    column: 8,
+    row: 11
+  },
+  {
+    key: 'incomeTax',
+    name: 'Income Tax',
+    column: 7,
+    row: 11
+  },
+  {
+    key: 'readingRailroad',
+    name: 'Reading Railroad',
+    isRailroad: true,
+    column: 6,
+    row: 11
+  },
+  {
+    key: 'frankenmuth',
+    name: 'Frankenmuth',
+    color: 'lightBlue',
+    column: 5,
+    row: 11
+  },
+  {
+    key: 'chanceBottom',
+    name: 'Chance',
+    column: 4,
+    row: 11
+  },
+  {
+    key: 'birchRun',
+    name: 'Birch Run',
+    color: 'lightBlue',
+    column: 3,
+    row: 11
+  },
+  {
+    key: 'saginaw',
+    name: 'Saginaw',
+    color: 'lightBlue',
+    column: 2,
+    row: 11
+  },
+  {
+    key: 'jail',
+    name: 'Jail',
+    isCornerSpace: true,
+    column: 1,
+    row: 11
+  }
+];
